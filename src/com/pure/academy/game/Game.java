bbb0c0f212a7dc20907a26fc055beac7eb580a3a
Tihomir Chobanov <tihomir.chobanov@gmail.com>
2022-02-10 20:51:05 +0200

    preparing initial logic

--- src/com/pure/academy/game/Game.java
+++ src/com/pure/academy/game/Game.java
@@ -1,15 +1,44 @@
 package com.pure.academy.game;
 
+import com.pure.academy.demo.DemoGame;
+
+import java.util.Scanner;
+
 public class Game {
-	
+	Scanner scanner = new Scanner(System.in);
+	int playerHP;
+	String playerName;
+	int choice;
+	int monsterHP;
+	int goldenKey;
+
+
 	public static void main(String args[]) {
 
-		System.out.println("Its Emo");
-		System.out.println("Its Tisho commit!");
-		System.out.println("Its Dobrina");
-		System.out.println("Its Victor");
-		
-		System.out.println("Its Plamen");
+        //numberOfRabbits and numberOfTrees
+		Game dublin = new Game();
+
+		dublin.playerSetup();
+		//dublin.townGate();
+
+
 	}
+
+	public void playerSetup() {
+		playerHP = 100;
+		monsterHP = 15;
+
+		//playerWeapon = "Knife";
+
+		System.out.println("Your HP: " + playerHP);
+		System.out.println("Please enter your name:");
+
+		playerName = scanner.nextLine();
+        // TODO: invalid input. Please enter alphabetic name of the character!
+		System.out.println("Hello " + playerName + ", let's start the game!");
+
+	}
+
+
 
 }
