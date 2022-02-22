46cbb2d72587183e05182aa818f3babf6a1c1bb8
Plamen Tenev <plamen.tenev.tenev@gmail.com>
2022-02-22 21:04:17 +0200

    adding backbone QuestionModel.java

---
+++ src/com/pure/academy/model/QuestionModel.java
@@ -0,0 +1,13 @@
+package com.pure.academy.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuestionModel {
+	
+	private String actualQuestion;
+	
+	private Map<String, Boolean> answerMap = new HashMap();
+	
+
+}
